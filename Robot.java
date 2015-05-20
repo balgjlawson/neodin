9a877fcbce3250342d3f7195e9dd41993fb40669
Khalil Jones <khaliljt416@msn.com>
2015-05-20 08:32:19 -0700

    Create Robot.java

---
+++ Robot.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc.team2928.robot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.RobotDrive.MotorType;
+import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.Timer;
+
+
+public class Robot extends SampleRobot 
+{
+	
+    
+		public Robot() 
+		{
+       
+		}
+        
+
+  
+		public void operatorControl()
+		{
+			
+		}
+}
+    
+
