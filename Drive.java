2387d9dd5a3ccb59daa48b0844e8bf78a8db3268
Khalil Jones <khaliljt416@msn.com>
2015-05-21 07:49:06 -0700

    Create Drive.java

---
+++ Drive.java
@@ -0,0 +1,65 @@
+package org.usfirst.frc.team2928.robot;
+
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+
+public class Drive {
+
+private Talon frontLeft; 
+private Talon frontRight;
+private Talon backLeft;
+private Talon backRight;
+Joystick driveJoy;
+RobotDrive odin;
+
+
+	public Drive(Talon t1, Talon t2, Talon t3, Talon t4, Joystick drive){
+		frontLeft =	t1;
+		frontRight = t2;
+		backLeft = t3;
+		backRight = t4;
+		driveJoy = drive;
+		odin = new RobotDrive(frontLeft,frontRight,backLeft, backRight );
+		
+	}
+	
+	public void go()
+	{
+		double pt1 = Math.pow(driveJoy.getX(), 3);
+		double pt1y = Math.pow(driveJoy.getY(), 3);
+		double pt2y =(4.0/5)*(pt1y);
+		double pt2x = (4.0/5)*(pt1);
+		double fixedX = 0;
+		double fixedY = 0;
+		if(driveJoy.getX()>.3)
+		{
+			fixedX = pt2x+.2;
+		}
+		
+		if(driveJoy.getX()<-.2)
+		{
+			fixedX = pt2x-.2;
+		}
+		
+		if(driveJoy.getY()>.3)
+		{
+			fixedY = pt2y+.2;
+		}
+		
+		if(driveJoy.getY()<-.2)
+		{
+			fixedY= pt2y-.2;
+		}
+		odin.arcadeDrive(fixedX, fixedY);
+	}
+	
+	public static void shift(Solenoid s1, Solenoid s2)
+	{
+			s1.set(!s1.get());
+			s2.set(!s2.get());
+	}
+	
+	
+}
